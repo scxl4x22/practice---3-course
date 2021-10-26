25e7bc7146689f97c1d3878f77b60c76f6b8283e
scxl4x22 <90278581+scxl4x22@users.noreply.github.com>
2021-10-26 17:17:45 +0300

    Add files via upload

---
+++ isp2281337 v.5 01.01/isp2281337 v.5 01.01/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace isp2281337_v._5_01._01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Triangle triangle = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculatePerimeter_Click(object sender, RoutedEventArgs e)
+        {
+            if (isThreeSides.IsChecked.Value == true)
+            {
+                try
+                {
+                    int sideA = Int32.Parse(inputSideA.Text);
+                    int sideB = Int32.Parse(inputSideB.Text);
+                    int sideC = Int32.Parse(inputSideC.Text);
+                    triangle.SetParams(sideA, sideB, sideC);
+                    perimeterOutput.Text = triangle.GetPerimeter().ToString();
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Введите длины сторон", "Ошибка");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show("Длина стороны не может быть отрицательной или равной 0", "Ошибка");
+                }
+            }
+            else if (isOneSide.IsChecked.Value == true)
+            {
+                try
+                {
+                    int newSide = Int32.Parse(inputSide.Text);
+                    triangle.SetParams(newSide);
+                    perimeterOutput.Text = triangle.GetPerimeter().ToString();
+                }
+                catch (FormatException)
+                {
+
+                    MessageBox.Show("Введите длины сторон", "Ошибка");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show("Длина стороны не может быть отрицательной или равной 0", "Ошибка");
+                }
+            }
+            else MessageBox.Show("Выберите способ ввода", "Ошибка");
+        }
+
+        private void isThreeSides_Click(object sender, RoutedEventArgs e)
+        {
+            if (isThreeSides.IsChecked.Value == true)
+            {
+                inputSideA.IsEnabled = true;
+                inputSideB.IsEnabled = true;
+                inputSideC.IsEnabled = true;
+
+            }
+            else
+            {
+                inputSideA.IsEnabled = false;
+                inputSideB.IsEnabled = false;
+                inputSideC.IsEnabled = false;
+            }
+        }
+
+        private void increaseBy2_Click(object sender, RoutedEventArgs e)
+        {
+            if (isThreeSides.IsChecked.Value == true)
+            {
+                try
+                {
+                    triangle.IncreaseByTwo();
+                    inputSideA.Text = triangle.SideA.ToString();
+                    inputSideB.Text = triangle.SideB.ToString();
+                    inputSideC.Text = triangle.SideC.ToString();
+                }
+                catch (ArgumentNullException)
+                {
+                    MessageBox.Show("Укажите длины сторон или рассчитайте периметр, чтобы продолжить", "Ошибка");
+                }
+            }
+            else if (isOneSide.IsChecked.Value == true)
+            {
+                try
+                {
+                    triangle.IncreaseByTwo();
+                    inputSide.Text = triangle.SideA.ToString();
+                }
+                catch (ArgumentNullException)
+                {
+                    MessageBox.Show("Укажите длины сторон или рассчитайте периметр, чтобы продолжить", "Ошибка");
+                }
+            }
+            else MessageBox.Show("Выберите способ ввода", "Ошибка");
+        }
+
+        private void isOneSide_Click(object sender, RoutedEventArgs e)
+        {
+            if (isOneSide.IsChecked.Value == true)
+                inputSide.IsEnabled = true;
+            else inputSide.IsEnabled = false;
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Галстян Владимир ИСП - 34\n" +
+                "Практическая работа №5\n" +
+                "Создать класс Triangle (треугольник) с полями-сторонами. Создать необходимые методы и свойства.Определить метод вычисления периметра. Создать " +
+                "перегруженные методы SetParams, для установки параметров объекта, в том числе увеличения размеров треугольника в 2 раза.", "О программе", MessageBoxButton.OK, MessageBoxImage.Information); ;
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            inputSide.Clear();
+            inputSideA.Clear();
+            inputSideB.Clear();
+            inputSideC.Clear();
+            perimeterOutput.Clear();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ isp2281337 v.5 01.01/isp2281337 v.5 01.01/Triangle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isp2281337_v._5_01._01
+{
+    class Triangle
+    {
+        private int sideA;
+        private int sideB;
+        private int sideC;
+
+        public int SideA
+        {
+            get
+            {
+                return sideA;
+            }
+            set
+            {
+                if (value < 0 || value == 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else sideA = value;
+            }
+        }
+
+        public int SideB
+        {
+            get
+            {
+                return sideB;
+            }
+            set
+            {
+                if (value < 0 || value == 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else sideB = value;
+            }
+        }
+
+        public int SideC
+        {
+            get
+            {
+                return sideC;
+            }
+            set
+            {
+                if (value < 0 || value == 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else sideC = value;
+            }
+        }
+
+        public void SetParams(int newSide)
+        {
+            SideA = newSide;
+            SideB = newSide;
+            SideC = newSide;
+        }
+
+        public void SetParams(int newSideA, int newSideB, int newSideC)
+        {
+            SideA = newSideA;
+            SideB = newSideB;
+            SideC = newSideC;
+        }
+
+        public int GetPerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public void IncreaseByTwo()
+        {
+            if (SideA != 0 && SideB != 0 && SideC != 0)
+            {
+                SideA *= 2;
+                SideB *= 2;
+                SideC *= 2;
+            }
+            else throw new ArgumentNullException();
+        }
+    }
+}
