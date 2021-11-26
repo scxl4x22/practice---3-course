17f0499f091a26e0642d3d3042eb82857c2843b8
scxl4x22 <90278581+scxl4x22@users.noreply.github.com>
2021-11-26 19:35:46 +0300

    Add files via upload

---
+++ isp2281337 v.7/isp2281337 v.7/Equilateral.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isp2281337_v._7
+{
+    class Equilateral : Triangle
+    {
+        public double GetArea(int a)
+        {
+            double p = (a + a + a) / 2.0;
+            double area = Math.Round(Math.Sqrt(p * (p - a) * (p - a) * (p - a)), 3);
+            if (area == Convert.ToDouble(area))
+                return area;
+            else
+                throw new ArgumentException();
+        }
+            
+        public double GetArea(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2.0;
+            double area = Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 3);
+            if (area == Convert.ToDouble(area))
+                return area;
+            else
+                throw new ArgumentException();
+        }
+    }
+}
