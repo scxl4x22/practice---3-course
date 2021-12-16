dc9ef50d3dd5e1064928f936f793bbdc45632ee0
scxl4x22 <as>
2021-12-16 20:44:10 +0300

    practice 13

---
+++ isp2281337 v.13 01.01/WpfLibrary1/BasicModules.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Data;
+using System.IO;
+
+namespace LibMatrix
+{
+    public static class VisualMatrix
+    {
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add($"{i + 1}", typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+    }
+
+    public class BasicMatrixActs
+    {
+        private Random _random = new Random();
+        private int[,] _matrix;
+
+        public int[,] Create(int lengthN, int lengthM, int min = 0, int max = 0)
+        {
+            _matrix = new int[lengthN, lengthM];
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(1); j++)
+                {
+                    _matrix[i, j] = _random.Next(min, max);
+                }
+            }
+            return _matrix;
+        }
+
+        public int[,] Generate(int lengthN, int lengthM, int min = -100, int max = 101)
+        {
+            _matrix = new int[lengthN, lengthM];
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(1); j++)
+                {
+                    _matrix[i, j] = _random.Next(min, max);
+                }
+            }
+            return _matrix;
+        }
+
+        public int[,] Open(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException();
+            }
+
+            StreamReader read = new StreamReader(path);
+            _matrix = new int[Convert.ToInt32(read.ReadLine()), Convert.ToInt32(read.ReadLine())];
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(1); j++)
+                {
+                    _matrix[i, j] = Convert.ToInt32(read.ReadLine());
+                }
+            }
+
+            return _matrix;
+
+        }
+
+        public void Save(string path)
+        {
+            {
+                StreamWriter write = new StreamWriter(path);
+                write.WriteLine(_matrix.GetLength(0));
+                write.WriteLine(_matrix.GetLength(1));
+                for (int i = 0; i < _matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < _matrix.GetLength(1); j++)
+                    {
+                        write.WriteLine(_matrix[i, j]);
+                    }
+                }
+                write.Close();
+            }
+        }
+
+        public void ClearMatrix(int max = 0)
+        {
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(1); j++)
+                {
+                    _matrix[i, j] = _random.Next(max);
+                }
+            }
+        }
+    }
+}
---
+++ isp2281337 v.13 01.01/isp2281337 v.13 01.01/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using LibMatrix;
+using Microsoft.Win32;
+
+namespace isp2281337_v._13_01._01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BasicMatrixActs matrixActs = new();
+        private OddColumn oddColumn = new();
+        private int[,] _matrix;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                    e.Handled = true;
+            }
+            else e.Handled = false;
+        }
+
+        private void input_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                DefaultExt = ".txt"
+            };
+
+            if (openFileDialog.ShowDialog() == false)
+            {
+                return;
+            }
+
+            _matrix = matrixActs.Open(openFileDialog.FileName);
+            inputRowCount.Text = Convert.ToString(_matrix.GetLength(0));
+            inputColumnCount.Text = Convert.ToString(_matrix.GetLength(1));
+            mainDataGrid.ItemsSource = _matrix.ToDataTable().DefaultView;
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                DefaultExt = ".txt"
+            };
+            if (mainDataGrid.ItemsSource != null)
+            {
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    matrixActs.Save(saveFileDialog.FileName);
+                }
+            }
+            else MessageBox.Show("Нечего сохранять", "Ошибка");
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            inputRowCount.Clear();
+            inputColumnCount.Clear();
+            matrixActs.ClearMatrix();
+            mainDataGrid.ItemsSource = VisualMatrix.ToDataTable(_matrix).DefaultView;
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №13\n" +
+                "Галстян Владимир ИСП-34\n" +
+                "Дана целочисленная матрица размера M * N. Найти номер первого из ее столбцов, содержащих только нечетные числа.Если таких столбцов нет, то вывести 0.", "О программе", MessageBoxButton.OK);
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void createMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int rowCount = Int32.Parse(inputRowCount.Text);
+                int columnCount = Int32.Parse(inputColumnCount.Text);
+                _matrix = matrixActs.Create(rowCount, columnCount);
+                mainDataGrid.ItemsSource = VisualMatrix.ToDataTable(_matrix).DefaultView;
+                matrixInfo.Content = $"Матрица {_matrix.GetLength(0)} x {_matrix.GetLength(1)}";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите данные", "Ошибка");
+            }
+        }
+
+        private void fillMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int rowCount = Int32.Parse(inputRowCount.Text);
+                int columnCount = Int32.Parse(inputColumnCount.Text);
+                _matrix = matrixActs.Generate(rowCount, columnCount);
+                mainDataGrid.ItemsSource = VisualMatrix.ToDataTable(_matrix).DefaultView;
+            }
+            catch
+            {
+                MessageBox.Show("Введите данные", "Ошибка");
+            }
+            matrixInfo.Content = $"Матрица {_matrix.GetLength(0)} x {_matrix.GetLength(1)}";
+        }
+
+        private void getOddColumn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_matrix != null)
+            {
+                int rowCount = Int32.Parse(inputRowCount.Text);
+                int columnCount = Int32.Parse(inputColumnCount.Text);
+                MessageBox.Show($"{oddColumn.GetColumn(_matrix)}");
+            }
+            else MessageBox.Show("Сгенерируйте таблицу", "Ошибка");
+        }
+    }
+}
